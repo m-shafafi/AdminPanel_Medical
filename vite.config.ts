2df038bfb93b945366bc464d417ee52b348446f3
m-shafafi <m-shafafi@outlook.com>
2025-09-27 17:56:26 +0330

    added proxy with test local

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(
-  {
-    server: {
-      host: '127.0.0.1'
-    },
-    plugins: [react(), tailwindcss(), tsconfigPaths()]
-  });
+export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      '/productsApi': 'http://127.0.0.1:20000',
+      '/newsApi': 'http://127.0.0.1:20003',
+      '/educationApi': 'http://127.0.0.1:20003',
+      '/galleryApi': 'http://127.0.0.1:20004'
+    }
+  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()]
+});
